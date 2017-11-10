851be284e1c3dc96f30193fca72fd93635818d27
Andris Strals <andris.strals@gmail.com>
2017-11-10 16:37:55 +0000

    Drawing as parametrisied function

--- ping-pong/src/Lib.hs
+++ ping-pong/src/Lib.hs
@@ -4,20 +4,55 @@ module Lib
 
 import Graphics.Gloss
 
+screenW::Float
+screenW = 600
+
+screenH::Float
+screenH = 480
+
+wallH::Float
+wallH = 10
+
+paddleLen::Float
+paddleLen = 80
+
+paddleThick::Float
+paddleThick = 20
+
 window :: Display
-window = InWindow "Pingy Pong" (200, 300) (10, 10)
+window = InWindow "Pingy Pong" (round screenW, round screenH) (10, 10)
 
 background :: Color
 background = light black
 
 drawing :: Picture
-drawing = pictures
-	[ translate (-20) (-100) $ color ballColor $ circleSolid 30
-	, translate 30 50 $ color paddleColor $ rectangleSolid 10 50
-	]
-	where
-		ballColor = dark red
-		paddleColor = light blue
+drawing = pictures [ ball
+				   , walls
+                   , mkPaddle rose (screenW /2 - paddleThick * 1.5) (-20)
+                   , mkPaddle orange (screenW /(-2) + paddleThick * 1.5) 40
+                   ]
+  where
+    --  The pong ball.
+    ball = translate (-10) 40 $ color ballColor $ circleSolid 10
+    ballColor = dark red
 
+    --  The bottom and top walls.
+    wall :: Float -> Picture
+    wall offset =
+      translate 0 offset $
+        color wallColor $
+          rectangleSolid screenW  wallH
+
+    wallColor = greyN 0.5
+    walls = pictures [wall (screenH /2 - wallH), wall (screenH /(-2) + wallH)]
+
+    --  Make a paddle of a given border and vertical offset.
+    mkPaddle :: Color -> Float -> Float -> Picture
+    mkPaddle col x y = pictures
+      [ translate x y $ color col $ rectangleSolid paddleThick paddleLen
+      , translate x y $ color paddleColor $ rectangleSolid (paddleThick - 6) (paddleLen - 6)
+      ]
+
+    paddleColor = light (light blue)
 showTheWin :: IO ()
 showTheWin = display window background drawing
