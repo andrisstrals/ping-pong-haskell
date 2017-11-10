ba80e5fc1f681907e0bea55a49aebcd87642e7c5
Andris Strals <andris.strals@gmail.com>
2017-11-10 15:02:20 +0000

    Paddle and ball in the cetre of the widow

--- ping-pong/src/Lib.hs
+++ ping-pong/src/Lib.hs
@@ -8,10 +8,16 @@ window :: Display
 window = InWindow "Pingy Pong" (200, 300) (10, 10)
 
 background :: Color
-background = white
+background = light black
 
 drawing :: Picture
-drawing = circle 80
+drawing = pictures
+	[ color ballColor $ circleSolid 30
+	, color paddleColor $ rectangleSolid 10 50
+	]
+	where
+		ballColor = dark red
+		paddleColor = light blue
 
 showTheWin :: IO ()
 showTheWin = display window background drawing
